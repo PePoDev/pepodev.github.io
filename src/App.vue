96020dd6e07b8d5e2dff06bd558d442bb5b15b6f
Thiwanon Chomcharoen <gmgame.1525@gmail.com>
2021-04-30 18:18:12 +0700

    :tada: init project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<Banner> </Banner>
+</template>
+
+<script lang="ts">
+import Banner from "./components/Banner.vue";
+
+export default {
+	components: { Banner },
+	setup() {
+		return {};
+	},
+};
+</script>
