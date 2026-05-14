12a4afa57af860b7527da6c16cbb9ea50711f079
PePoDev <pepo@pepo.dev>
2026-05-15 02:01:12 +0700

    feat: add resume app and enhance mobile responsiveness across various components

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,9 @@ export default defineConfig({
     inlineStylesheets: "auto",
   },
   compressHTML: true,
+  devToolbar: {
+    enabled: false,
+  },
   image: {
     domains: [],
     remotePatterns: [],
